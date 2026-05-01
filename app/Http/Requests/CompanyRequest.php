ac2513c6814eca1d250b46bcbd3b15a06c6a6a12
Adam <35268127+Zewodec@users.noreply.github.com>
2026-05-01 15:33:19 +0300

    feat: Create migration and model for companies with validation request

---
+++ app/Http/Requests/CompanyRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CompanyRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'name'    => ['required', 'string', 'max:256'],
+            'edrpou'  => ['required', 'string', 'max:10'],
+            'address' => ['required', 'string'],
+        ];
+    }
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+}
